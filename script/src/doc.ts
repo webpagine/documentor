4b87b4433df419746cf1691132ebdb1a15c98c79
Jelly Terra <jellyterra@proton.me>
2024-09-02 23:34:29 +0800

    Add workflow for Pagine v2.2.0. Set `min-height` to `100vh` for `main` element in `doc.html`

--- script/src/doc.ts
+++ script/src/doc.ts
@@ -1,6 +1,32 @@
 function initDoc() {
     generateSidenav()
     generateBlockquotes()
+
+    window.addEventListener("scroll", scrollEvent)
+}
+
+function scrollEvent() {
+    let elements = document.getElementsByTagName("main")[0].querySelectorAll("h1,h2,h3")
+
+    elements.forEach(element => {
+        element.classList.remove("current")
+        document.getElementById(`sidenav-${element.id}`).classList.remove("current")
+    })
+
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i]
+
+        if (element.getBoundingClientRect().top < 0) continue
+
+        element.classList.add("current")
+        document.getElementById(`sidenav-${element.id}`).classList.add("current")
+
+        break
+    }
+}
+
+function generateIdFromElement(element: Element) {
+    return element.innerHTML.toLowerCase().replace(/[^a-zA-Z\s-]/g, "").replaceAll(" ", "-")
 }
 
 function generateSidenav() {
@@ -9,26 +35,27 @@ function generateSidenav() {
     let h1id = ""
     let h2id = ""
 
-    document.querySelector(".content").querySelectorAll("h1,h2,h3").forEach((element)=> {
-        let h1 = root[root.length-1]
+    document.querySelector(".content").querySelectorAll("h1,h2,h3").forEach((element) => {
+        let h1 = root[root.length - 1]
 
         switch (element.tagName) {
             case "H1":
-                element.id = element.innerHTML.toLowerCase().replaceAll(" ", "-")
+                element.id = generateIdFromElement(element)
                 h1id = element.id
 
                 root.push({id: element.id, text: element.innerHTML, h2: []})
+
                 break
             case "H2":
-                element.id = `${h1id}-${element.innerHTML.toLowerCase().replaceAll(" ", "-")}`
+                element.id = `${h1id}-${generateIdFromElement(element)}`
                 h2id = element.id
 
                 h1.h2.push({id: element.id, text: element.innerHTML, h3: []})
                 break
             case "H3":
-                element.id = `${h2id}-${element.innerHTML.toLowerCase().replaceAll(" ", "-")}`
+                element.id = `${h2id}-${generateIdFromElement(element)}`
 
-                h1.h2[h1.h2.length-1].h3.push({id: element.id, text: element.innerHTML})
+                h1.h2[h1.h2.length - 1].h3.push({id: element.id, text: element.innerHTML})
                 break
         }
     })
@@ -36,11 +63,11 @@ function generateSidenav() {
     let sidenav = document.getElementById("sidenav")
     sidenav.innerHTML += "<ul>"
     for (let h1 of root) {
-        sidenav.innerHTML += `<li><a href="#${h1.id}"></a></li><ul>`
+        sidenav.innerHTML += `<li><a id="sidenav-${h1.id}" href="#${h1.id}"></a></li><ul>`
         for (let h2 of h1.h2) {
-            sidenav.innerHTML += `<li style="padding-left: 20px"><a href="#${h2.id}">${h2.text}</a></li><ul>`
+            sidenav.innerHTML += `<li id="sidenav-${h2.id}" style="padding-left: 20px"><a href="#${h2.id}">${h2.text}</a></li><ul>`
             for (let h3 of h2.h3) {
-                sidenav.innerHTML += `<li style="padding-left: 40px"><a href="#${h3.id}">${h3.text}</a></li>`
+                sidenav.innerHTML += `<li id="sidenav-${h3.id}" style="padding-left: 40px"><a href="#${h3.id}">${h3.text}</a></li>`
             }
             sidenav.innerHTML += "</ul>"
         }
